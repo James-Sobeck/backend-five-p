b23f7b25cf40a2475fd31fe903c554cb706497ab
James Sobeck <sobeckjames@gmail.com>
2022-05-28 17:18:21 -0500

    .

--- src/db/migrations/20220421232954_Modules.js
+++ src/db/migrations/20220421232954_Modules.js
@@ -6,7 +6,7 @@ exports.up = function(knex) {
   return knex.schema.createTable("modules", (table)=>{
       table.increments("module_id").primary();
       table.integer("user_id");
-      table.foreign("user_id").references("user_id").inTable("users")
+      table.foreign("user_id").references("user_id").inTable("users");
       table.dateTime("finished_at");
   })
 };
