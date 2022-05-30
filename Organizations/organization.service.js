0d0b89f856adf1af2e4a544d98c91ecdd74ce96a
James Sobeck <sobeckjames@gmail.com>
2022-05-30 02:42:09 -0500

    .

--- Organizations/organization.service.js
+++ Organizations/organization.service.js
@@ -1,7 +1,7 @@
 const knex = require("../src/db/connection")
 
 function create(organization){
-    return knex("organization")
+    return knex("orgranizations")
     .insert(organization)
     .returning("*")
     .then((newOrg)=> newOrg[0])
